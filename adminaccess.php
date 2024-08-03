f7caf2c1af80b825d4f2f6b2d64513302328cba8
Satnam Singh <kharoudsatnam9@gmail.com>
2024-08-03 17:06:27 -0400

    update adminaccess.php

--- adminaccess.php
+++ adminaccess.php
@@ -6,38 +6,24 @@ $pass =  $_POST['adminpwd'];
 
 
 //Check username
-$r = $conn->query("SELECT * FROM logins WHERE username = '".$user_name."'");
+// $r = $conn->query("SELECT * FROM adminLogin WHERE userName = '$user_name'");
 
-if ($r->num_rows != 1) {
-    echo '<script>alert("Usernme does not exist.")</script>'; 
-    include('index.php');
-}else{
-    $r = $conn->query("SELECT * FROM logins WHERE username = '".$user_name."' AND password = '".$pass."'");
-    if ($r->num_rows != 1) {
-        echo '<script>alert("Invalid password. Try again.")</script>'; 
+$resultlog = mysqli_query($conn,"SELECT * from adminLogin") or die(mysqli_error($conn));
+$row = mysqli_fetch_array($resultlog);
+
+if ($row[2] != $user_name){
+
+    if($row[3] != $pass) {
+        include('admindashboard.php');
+    } else{
+        echo '<script>alert("Incorrect Password.");</script>'; 
         include('adminlogin.php');
-    }else{
-        $r = $conn->query("SELECT * FROM logins WHERE username = '".$user_name."' AND password = '".$pass."' AND status = 'Active'");
-        if ($r->num_rows != 1) {
-            echo '<script>alert("Your login is not Active. Kindly contact your manager!")</script>'; 
-            include('index.php');
-            }
-        else{
-            $r = $conn->query("SELECT * FROM logins WHERE username = '".$user_name."' AND password = '".$pass."' AND super ='Yes'");
-            if ($r->num_rows != 1) {
-                echo '<script>alert("Your login does not have Admin privilage. Kindly use regular login page!")</script>'; 
-                include('index.php');
-                }
-            else{
-                include('admindashboard.php');
-            };
-        };    
-    };
-};
+    }
 
-//session_start();
-//$_SESSION['username'] = $user_name;
+} else {
+    echo '<script>alert("Usernme does not exist.");</script>'; 
+    include('adminlogin.php');
+}
 
-//$conn->close();
 ?>
 
